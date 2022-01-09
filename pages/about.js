d564244a13f61a228db1e41cde3aaa3cae6c8cc8
Mrunal Saoji <mrunalsaoji@gmail.com>
2022-01-09 06:26:09 +0000

    initialized the app with about and event pages

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+export default function about() {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>This is an app to find the lastest DJ and other musical events</p>
+            <p>version 1.0</p>
+        </div>
+    )
+}
+
