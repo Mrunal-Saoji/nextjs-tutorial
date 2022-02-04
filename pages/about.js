e8bc9f28c80c77c794ea4676a855b582a9bcc9bf
Mrunal Saoji <mrunalsaoji@gmail.com>
2022-02-04 13:28:38 +0000

    Create Layout, header and Footer.

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,12 @@
+import Layout from "../components/Layout"
+
 export default function about() {
     return (
-        <div>
+        <Layout>
             <h1>About</h1>
             <p>This is an app to find the lastest DJ and other musical events</p>
             <p>version 1.0</p>
-        </div>
+        </Layout>
     )
 }
 
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,8 @@
+import Layout from "../../components/Layout"
 export default function index() {
     return (
-        <div>
+        <Layout>
             <h1>Events</h1>
-        </div>
+        </Layout>
     )
 }
